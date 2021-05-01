8c613c1683d2b58286b7116aba0bf1f1b97f6bb6
Sophie Gu <sophieqgu@gmail.com>
2021-05-01 18:06:12 -0400

    Create Main and Profile components

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Profile from './Profile'
+
+class Main extends Component {
+   render() {
+       return (
+           <div className="main">
+               <Profile />
+           </div>
+       );
+   }
+}
+
+export default Main;
