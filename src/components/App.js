de7fee5b1265fbfe7f8d1949cb5077b05d34588b
Sophie Gu <sophieqgu@gmail.com>
2021-05-01 17:21:55 -0400

    Install NBA API and create TopNavBar component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopNavBar from './TopNavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavBar />
+    </div>
+  );
+}
+
+export default App;
