eb495a7d8df4fa1232666a7dacb01b3aa0f5df68
Sophie Gu <sophieqgu@gmail.com>
2021-05-02 15:06:27 -0400

    Install ant design and lodash library

--- src/components/ShotChart.js
+++ src/components/ShotChart.js
@@ -8,23 +8,31 @@ import PropTypes from 'prop-types';
 window.d3_hexbin = {hexbin : hexbin};
 
 class ShotChart extends Component {
+
+    static propTypes = {
+      playerId: PropTypes.number,
+      minCount: PropTypes.number,
+      chartType: PropTypes.string,
+      displayTooltip: PropTypes.bool,
+    }
+
     componentDidMount() {
-        nba.stats.shots({
-          PlayerID: this.props.playerId
-        }).then(response => {
-          const final_shots = response.shot_Chart_Detail.map(shot => ({
-            x: (shot.locX + 250) / 10,
-            y: (shot.locY + 50) / 10,
-            action_type: shot.actionType,
-            shot_distance: shot.shotDistance,
-            shot_made_flag: shot.shotMadeFlag,
-          }));
-          const courtSelection = d3.select("#shot-chart");
-          const chart_court = court().width(500);
-          const chart_shots = shots().shotRenderThreshold(2).displayToolTips(true).displayType("hexbin");
-          courtSelection.call(chart_court);
-          courtSelection.datum(final_shots).call(chart_shots);
-        });
+      nba.stats.shots({
+        PlayerID: this.props.playerId
+      }).then(response => {
+        const final_shots = response.shot_Chart_Detail.map(shot => ({
+          x: (shot.locX + 250) / 10,
+          y: (shot.locY + 50) / 10,
+          action_type: shot.actionType,
+          shot_distance: shot.shotDistance,
+          shot_made_flag: shot.shotMadeFlag,
+        }));
+        const courtSelection = d3.select("#shot-chart");
+        const chart_court = court().width(500);
+        const chart_shots = shots().shotRenderThreshold(2).displayToolTips(true).displayType("hexbin");
+        courtSelection.call(chart_court);
+        courtSelection.datum(final_shots).call(chart_shots);
+      });
     }
 
     render() {
