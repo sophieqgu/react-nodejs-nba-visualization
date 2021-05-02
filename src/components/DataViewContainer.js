82c7f48553fd69a0f93786b5b6dd7e2ed5ea13c2
Sophie Gu <sophieqgu@gmail.com>
2021-05-02 15:40:42 -0400

    Add CounterSlider component to dynamically change shotRenderThreshold

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
 
 import ShotChart from './ShotChart';
+import CounterSlider from './CounterSlider'
 
 class DataViewContainer extends Component {
+    state = {
+      minCount: 2
+    }
+
+    onCountSliderChange = (count) => {
+      this.setState({ minCount: count });
+    }
+
    render() {
        return (
            <div className="data-view">
                <ShotChart playerId={this.props.playerId}/>
+               <CounterSlider
+                  value={this.state.minCount}
+                  onCountSliderChange={_.debounce(this.onCountSliderChange, 500)}
+               />
            </div>
        );
    }
