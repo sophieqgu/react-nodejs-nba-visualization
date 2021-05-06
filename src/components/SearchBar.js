54e61406e479edb3426f1a54fe954fee75c9dfc6
Sophie Gu <sophieqgu@gmail.com>
2021-05-05 21:35:27 -0400

    Add autocomplete to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
+import { AutoComplete, Input, Icon } from 'antd';
 
 class SearchBar extends Component {
-   render() {
-       return (
-           <div>
-               search bar
-           </div>
-       );
-   }
+  state = {
+      dataSource: [],
+  }
+
+  render() {
+      const { dataSource } = this.state;
+
+      return (
+          <AutoComplete
+              className="search-bar"
+
+              onSelect={this.onSelect}
+              onSearch={this.handleSearch}
+              placeholder="Search NBA Player"
+              size="large"
+              optionLabelProp="value"
+          >
+
+          <Input suffix={<Icon type="search" className="certain-category-icon" />} />
+          </AutoComplete>
+      );
+  }
+
 }
 
 export default SearchBar;
